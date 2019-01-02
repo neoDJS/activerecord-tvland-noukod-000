ca70bff422218151da8c8597c26cdb3ad4ade298
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-02 19:40:06 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_name_and_genre_to_show.rb
+++ db/migrate/009_add_name_and_genre_to_show.rb
@@ -1,4 +1,4 @@
-class AddNameAndGenreToShow < ActiveRecord::Migration
+class AddNameAndGenreToShow < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :name, :string
     add_column :shows, :genre, :string
