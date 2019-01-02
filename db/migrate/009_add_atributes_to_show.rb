0c825ab66ac05307f3021d20aa761702a0ff77ce
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-02 19:46:31 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_atributes_to_show.rb
+++ db/migrate/009_add_atributes_to_show.rb
@@ -1,4 +1,4 @@
-class AddAttributeToShow < ActiveRecord::Migration[4.2]
+class AddAttributesToShow < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :genre, :string
     add_column :shows, :day, :string
