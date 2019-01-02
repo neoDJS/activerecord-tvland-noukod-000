56ac9ba2de51dc1d7039b344a16f849cee405d1a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-02 19:55:40 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,6 +3,6 @@ class Character < ActiveRecord::Base
   belongs_to :actor
 
   def say_that_thing_you_say
-    "#{Actor.full_name} always says: #{catchphrase}"
+    "#{actor.full_name} always says: #{catchphrase}"
   end
 end
