8a0dce71893830587681f7c4c0e52ab71019b72b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-02 19:38:48 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,4 +1,4 @@
-class CreateActors < ActiveRecord::Migration
+class CreateActors < ActiveRecord::Migration[4.2]
   def change
     create_table :actors do |t| #we get a block variable here for the table
       #primary key of :id is created for us!
