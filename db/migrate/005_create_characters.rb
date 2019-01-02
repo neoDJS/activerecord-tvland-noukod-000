04dbfbdfab5e968d2faf06ac56c044ee43dd1e25
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-02 19:57:44 +0000

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,9 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+  def change
+    create_table :characters do |t| #we get a block variable here for the table
+      #primary key of :id is created for us!
+      # defining columns is as simple as t.[datatype] :column
+      t.string :name
+    end
+  end
+end
